254d0be4b1edfc09c5d39c1edb00a30ac5c00669
Sofia Isabel <sofiabravo103@gmail.com>
2013-05-27 19:58:30 -0430

    agregar noticias, login usuarios, botones lindos

--- main/views.py
+++ main/views.py
@@ -1,13 +1,82 @@
 from django.shortcuts import render_to_response
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.template import Template, RequestContext
+
 from models import *
 import datetime
 
 def index(request):
     noticias = Noticia.objects.filter()
-    return render_to_response('index.html',{'noticias': noticias})
+    state = rol = ''
+    state = check(request)
+    if state == 'in':	
+        if not request.user.is_superuser:
+            rol = 'usuario'
+        else:
+            rol = 'administrador'
+    return render_to_response('index.html',{'noticias': noticias,'rol':rol,'state':state},context_instance=RequestContext(request))
 
-def login(request):
-    return render_to_response('login.html')
+def logout_user(request):
+    state = check(request)
+    if state == 'in':	
+	username = request.user.username
+        logout(request)
+        state = 'out'
+	mensaje =  username + ' cerro sesion exitosamente'
+    else: 
+        mensaje = 'Warning'
+    return render_to_response('login.html',{'state':state,'mensaje':mensaje},context_instance=RequestContext(request))
+    
+
+def check(request):
+    if request.user.is_authenticated():
+        return 'in'
+    else:
+        return 'out'
+
+
+def login_user(request):
+    state = check(request)
+    username = password = mensaje = ''
+    if state == 'in':
+        mensaje = "Usuario: " + request.user.username
+    if request.POST:
+        if state == 'in':
+            username = request.user.username
+            mensaje = "Usuario: " + username
+        else:    
+            mensaje = "Por favor introduce tus datos..."
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    mensaje = username + " inicio sesion exitosamente"
+                    state= 'in'
+                else:
+                    mensaje = 'Su cuenta ha sido desabilitada. Comuniquese con un miembro del LDAC'
+                    state = 'out'
+            else:
+                state = 'out'
+                mensaje = "Su contrasena y/o usuario son incorrectos. Intente de nuevo."
+                
+    return render_to_response('login.html',{'state':state, 'username': username, 'mensaje': mensaje},context_instance=RequestContext(request))
+
+def nueva_noticia(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        contenido = request.POST.get('contenido')
+        Noticia.objects.create(titulo=titulo,contenido=contenido)
+    return index(request)
+
+def limpiar_noticias(request):
+    if request.POST:
+        Noticia.objects.create(titulo=titulo,contenido=contenido)
+    return index(request)
+    
+
 def nosotros(request):
     return render_to_response('nosotros.html')
 def horario(request):
